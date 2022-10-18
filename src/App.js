7d5f431f50b801b72b14e4e4800930227e11aca8
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-18 21:28:15 +0100

    my calc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Calculator from './components/Calculator';
+import "./App.css";
+import Calculator from "./components/Calculator";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-    <Calculator />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>MyPurpleCalculator</title>
+        <link rel="canonical" href="" />
+        <meta name="description" content="a cute purple calculator" />
+      </Helmet>
+      <Calculator />
     </div>
   );
 }
